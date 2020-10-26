4c48bf597396d63bb4a680aa68ec90268318bd73
ch33233 <jchcaksj2@gmail.com>
2020-10-27 07:57:03 +0900

    .

--- test/router/main.js
+++ test/router/main.js
@@ -1,9 +1,44 @@
-module.exports = function(app)
-{
-     app.get('/',function(req,res){
-        res.render('index.html')
-     });
-     app.get('/about',function(req,res){
-        res.render('about.html');
-    });
+var express = require('express');
+var session = require('express-session');
+
+module.exports = function(app) {
+  app.get('/', (req,res) => {
+    res.render('main.html')
+  });
+
+  app.get('/mypage', (req,res) => {
+    res.render('mypage.html');
+  });
+
+  app.get('/plnner', (req,res) => {
+    res.render('planner.html');
+  });
+
+  app.get('/statistics', (req,res) => {
+    res.render('statistics.html');
+  });
+
+  app.post('/login', (req,res) => {
+    res.render('about.html');
+  });
+  
+  app.post('/logout', (req,res) => {
+    res.render('about.html');
+  });
+
+  app.use(session({
+    secret: 'alskdjfalkd123',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+  app.get('/count', (req, res) => {
+    if(req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send('count : ' + req.session.count);
+  })
+
 }
--- test/server.js
+++ test/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
